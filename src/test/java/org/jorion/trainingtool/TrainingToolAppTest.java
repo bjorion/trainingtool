d7c0c91acb2c6ffd151a225d0691dae9590e2463
bjorion <bjorion@gmail.com>
2024-11-17 21:19:04 +0100

    feature(architecture): add spring modulith

--- src/test/java/org/jorion/trainingtool/TrainingToolAppTest.java
+++ src/test/java/org/jorion/trainingtool/TrainingToolAppTest.java
@@ -1,12 +1,16 @@
 package org.jorion.trainingtool;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Slf4j
 @ActiveProfiles("local")
 @SpringBootTest(classes = TrainingToolApp.class)
 public class TrainingToolAppTest {
@@ -16,6 +20,24 @@ public class TrainingToolAppTest {
      */
     @Test
     void contextLoads(ApplicationContext context) {
+
         assertNotNull(context);
+
+        var modules = ApplicationModules.of(TrainingToolApp.class);
+        new Documenter(modules)
+                .writeIndividualModulesAsPlantUml()
+                .writeModulesAsPlantUml()
+                //.writeModuleCanvases()
+        ;
+        // modules.verify();
+    }
+
+    @SuppressWarnings("unused")
+    void listModules() {
+
+        var modules = ApplicationModules.of(TrainingToolApp.class);
+        for (var m : modules) {
+            log.info("Name [{}], Base Package [{}]", m.getName(), m.getBasePackage());
+        }
     }
 }
